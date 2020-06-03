6fe2e92dd8e83cf266caab814442fb5fe7b655ce
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2020-06-02 22:28:50 -0300

    CRUD completo ok

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,9 +6,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -18,8 +24,11 @@ class UserController extends Controller
     {
         //$users = User::all();
         $users = User::paginate(8); //Para adicionar paginação com o Laravel. Deve usar a função links() na view.
+        $loggedId = Auth::id();
+       
         return view('admin.users.index', [
-            'users'=> $users
+            'users'=> $users,
+            'loggedId' => $loggedId
         ]);
     }
 
@@ -48,7 +57,7 @@ class UserController extends Controller
             'password_confirmation'
         ]);
 
-        $validator = $this->validator($data);
+        $validator = $this->validatorCreate($data);
 
         if($validator->fails()){
             return redirect()->route('users.create')
@@ -79,7 +88,15 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+
+        if($user){
+            return view('admin.users.edit', [
+                'user'=> $user
+            ]);
+        }
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -91,7 +108,67 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        if($user){
+            $data = $request->only([
+                'name',
+                'email',
+                'password',
+                'password_confirmation'
+            ]);
+
+            $validator = Validator::make( [
+                'name' => $data['name'],
+                'email' => $data['email']
+            ], [
+                'name' => ['required', 'string', 'max:100'],
+                'email' => ['required', 'string', 'email', 'max:100']
+            ]);
+
+            // Feitas as validações agora altera o nome
+            $user->name = $data['name'];
+            // Verifica se o email foi alterado
+            if($user->email != $data['email']){
+                $hasEmail = User::where('email', $data['email'])->get();//Verifica se o novo email já existe
+                if(count($hasEmail) === 0){
+                    $user->email = $data['email'];
+                }else{
+                    $validator->errors()->add('email', __('validation.unique', [
+                        'attribute' => 'email'
+                    ]));
+                    
+                }
+            }
+            //Verifica se o usuário digitou senha para alterar ela também
+            if(!empty($data['password'])){
+                if(strlen($data['password']) >= 4){
+                    if($data['password'] === $data['password_confirmation']){
+                        $user->password = Hash::make($data['password']);
+                    }else{
+                        $validator->errors()->add('password', __('validation.confirmed', [
+                            'attribute' => 'password'
+                        ]));
+                    }
+                }else{
+                    $validator->errors()->add('password', __('validation.min.string', [
+                        'attribute' => 'password',
+                        'min' => 4
+                    ]));
+                    
+                }
+
+            }
+            
+            if(count($validator->errors()) > 0){
+                return redirect()->route('users.edit', [
+                    'user' => $id
+                ])->withErrors($validator);
+            }
+
+            $user->save();
+        }
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -102,10 +179,17 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $loggedId = Auth::id();
+
+        if($loggedId != $id){
+            $user = User::find($id);
+            $user->delete();
+        }
+
+        return redirect()->route('users.index');
     }
 
-    protected function validator(array $data){
+    protected function validatorCreate(array $data){
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:100'],
             'email' => ['required', 'string', 'email', 'max:200', 'unique:users'],
