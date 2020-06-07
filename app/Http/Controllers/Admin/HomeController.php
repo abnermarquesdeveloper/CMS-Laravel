9284366a2b3ffcc1854b14562a5cb8027015443e
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2020-06-07 12:16:09 -0300

    Página dashboard sem Grafico

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Visitor;
+use App\User;
+use App\Page;
 
 class HomeController extends Controller
 {
@@ -14,6 +17,30 @@ class HomeController extends Controller
 
 
     public function index(){
-        return view ('admin.home');
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+
+        //Contagem de visitantes
+        $visitsCount = Visitor::count();
+
+        //Contagens de usuários online
+        $datelimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')->where('date_access','>=', $datelimit)->groupBy('ip')->get(); //gruupBy para saber quantos usuários distintos estão online no momento e/ou até pelo menos 5 minutos atrás
+        $onlineCount = count($onlineList);
+
+        //Contagem de páginas
+        $pageCount = Page::count();
+
+        //Contagem de usuários
+        $userCount = User::count();
+        
+        return view ('admin.home', [
+            'visitsCount' => $visitsCount,
+            'onlineCount' => $onlineCount,
+            'pageCount'   => $pageCount,
+            'userCount'   => $userCount
+        ]);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,9 +5,79 @@
 
 
 @section('content_header')
-    <h1>Painel de Controle</h1>
+    <h1>Dashboard</h1>
 @endsection
 
 @section('content')
-    Aqui fica todo o conteúdo do site ou sistema
+    
+    <div class="row">
+        <div class="col-md-3">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{$visitsCount}}</h3>
+                    <p>Acessos</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-eye"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{$onlineCount}}</h3>
+                    <p>Usuários Online</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-heart"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{$pageCount}}</h3>
+                    <p>Páginas</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-sticky-note"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{$userCount}}</h3>
+                    <p>Usuários</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-user"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Páginas mais visitadas</h3>
+                </div>
+                <div class="card-body">
+                    Texto do corpo...
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Sobre o Sistema</h3>
+                </div>
+                <div class="card-body">
+                    Texto sobre o Sistema
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
